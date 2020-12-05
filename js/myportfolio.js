a39672f33c39025ad195b56a24eb8058a4215300
Karthik Venkatasivareddy <32982164+karthikv142@users.noreply.github.com>
2020-12-05 02:09:33 -0600

    Add files via upload

---
+++ js/myportfolio.js
@@ -0,0 +1,26 @@
+const burger=document.querySelector(".burger");
+const nav=document.querySelector(".nav-links");
+const navlinks=document.querySelectorAll('.nav-links li');
+const bannerarea=document.querySelector('.banner-area');
+
+function disp_nav(){
+  nav.classList.toggle('nav-active');
+  navlinks.forEach((links, i) => {
+    if(links.style.animation){
+        links.style.animation='';
+    }
+    else{
+      links.style.animation=`navLinkFade 0.5s ease forwards ${i/1.5}s`;
+    }
+  });
+
+}
+
+function navslide(){
+
+    burger.addEventListener('click',disp_nav);
+
+}
+
+
+navslide();
